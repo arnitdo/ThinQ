a58296afad04ed4961b4fd77c9da0e2ad761f9e2
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2024-03-21 22:12:57 +0530

    added s3 credentials to env and code

--- flask/constants.py
+++ flask/constants.py
@@ -6,14 +6,25 @@ S3_BUCKET = os.getenv('S3_BUCKET')
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
-
-
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-s3 = boto3.client('s3',config=boto3.session.Config(signature_version='s3v4'))
+s3 = boto3.client(
+    's3',
+    region_name='ap-south-1',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    config=boto3.session.Config(signature_version='s3v4')
+)
 
-ses_client = boto3.client('ses')
+ses_client = boto3.client(
+    'ses',
+    region_name='ap-south-1',
+    aws_access_key_id=AWS_SECRET_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
 
 tokenizer = AutoTokenizer.from_pretrained("vennify/t5-base-grammar-correction")
 
