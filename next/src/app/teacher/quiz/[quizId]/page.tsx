8c68a0329e1b08a706cd78b86c850241251316c0
arnitdo <arnav.nitin.deo@gmail.com>
2024-03-27 17:39:44 +0530

    Add teacher files

---
+++ next/src/app/teacher/quiz/[quizId]/page.tsx
@@ -0,0 +1,13 @@
+// Individual Quiz Dash
+
+/*
+* Graphs and stuff
+* No tabular option, graphs only
+* */
+
+export default function Qizzes(){
+	return (
+		<>
+		</>
+	)
+}
---
+++ next/src/app/teacher/quiz/page.tsx
@@ -0,0 +1,15 @@
+// List quizzes
+
+/*
+* Quiz Name
+* No of Questions
+* No of Responses
+* Time Generated
+* */
+
+export default function Qizzes(){
+	return (
+		<>
+		</>
+	)
+}
