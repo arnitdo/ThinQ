7633a5e449b65167e80fab85402ee09c390f6f9d
prink <prinkaldoshi@gmail.com>
2024-03-30 00:10:33 +0530

    resources layour done

--- next/src/app/(pages)/student/classrooms/[classroomId]/resources/page.tsx
+++ next/src/app/(pages)/student/classrooms/[classroomId]/resources/page.tsx
@@ -6,6 +6,8 @@ import useAuthStore from '@/lib/zustand'
 import { getLectures} from '@/util/client/helpers'
 import { Lecture} from '@prisma/client'
 import Link from 'next/link'
+import { getAllResources } from '@/util/client/helpers'
+import { Resources } from '@prisma/client'
 import {useEffect, useState} from 'react'
 
 type ClassCardProps = {
@@ -15,6 +17,17 @@ type ClassCardProps = {
 const Page = ({params: {classroomId}}: {params: {classroomId: string}}) => {
 	
 	
+	const {user} = useAuthStore()
+	const [data, setData] = useState<Resources[]>([]);
+
+	useEffect(() => {
+		const getData = async () => {
+			if (!user) return;
+			const quizzes = await getAllResources(user.userOrgId, classroomId)
+			if (quizzes) setData(quizzes)
+		}
+		getData()
+	}, [user])
 
 	
 	const navlinks: NavLink[] = [
@@ -32,19 +45,34 @@ const Page = ({params: {classroomId}}: {params: {classroomId: string}}) => {
 		},
         {
             href: `/student/classrooms/${classroomId}/resources`,
-			title: "Notes"
+			title: "Resources"
         }
 	
 	]
-	return (
-		<>
-			<NestedNav items={navlinks} button={(<></>)}/>
-
-			<main className='py-4'>
-				resources
-			</main>
-		</>
-	)
+	const ResourceCard = ({item}: {item: Resources}) => {
+        
+		return (
+			<Link href={`/student/classrooms/${classroomId}/quiz/${item.quizId}`} className='quizCard | rounded-[0.625rem] border border-[#A0A0A0] text-center px-6 py-7'>
+        <h1 className='text-xl text-black'>{item.quizName}</h1>
+        <p className='mt-3'>Tap to reveal answers</p>
+        <div className='mt-3 flex gap-3 flex-wrap justify-center'>
+          <p className='text-sm text-[#0039C6] border border-[#5462DF] bg-[#CCE0FF] font-medium py-[0.375rem] px-3 rounded-full'>Download</p>
+          <p className='text-sm text-[#00802B] border border-[#00B833] bg-[#CCFFE0] font-medium py-[0.375rem] px-3 rounded-full'>Share7</p>
+        </div>
+      </Link>
+		)
+	}
+  return (
+    <div className=' flex flex-col gap-2'>
+			<NestedNav items={navlinks} button={(<></>)}/>  
+	<div>
+    <h1 className='text-4xl text-black font-medium'>Resources</h1>
+    <div className='quizCardWrapper | mt-3 grid gap-10 grid-cols-[repeat(auto-fill,minmax(350px,1fr))]'>
+      {data.map((item) => (<ResourceCard item={item} key={item.quizId}/>))}
+    </div>
+	</div>
+    </div>
+  )
 }
 
 export default Page
